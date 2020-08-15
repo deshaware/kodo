b7c702a29a8d5008fd3a722ef207864edaf32828
Swapnil Deshaware <deshaware.swapnil@gmail.com>
2020-08-15 21:31:37 +0530

    get data created

--- server/src/router/entities.js
+++ server/src/router/entities.js
@@ -5,7 +5,25 @@ const service = require('../service')
 
 router.get('/getData', async ( req, res ) => {
     try {
-        console.log("hi")
+        const { search, sortBy, orderBy, limit, skip } = req.query;
+
+        let select = (search) ? `Select * from data where name ilike '%${search}%' or description ilike '%${search}%'` : `Select * from data`;
+
+        let sort = ( sortBy && sortBy.toLowerCase() === 'name') ? (` order by name ${orderBy && orderBy === 'desc' ? 'desc' : 'asc'}`) :
+            ( sortBy && sortBy.toLowerCase() === 'datelastedited' ) ? (` order by name ${orderBy && orderBy === 'desc' ? 'desc' : 'asc'}`) : ` order by name ${orderBy && orderBy === 'desc' ? 'desc' : 'asc'}` ;
+
+        let limitRecord = (limit && limit > 0) ? ` LIMIT ${limit}` : ' LIMIT ALL';
+        let offset = (skip && skip > 0) ? ` OFFSET ${skip}` : ' OFFSET 0';
+
+        const query = select + sort + limitRecord + offset;
+        console.log(query)
+        let result = await pg.query(query)
+        res.status(200).send({
+            status: 'SUCCESS',
+            message: 'Data fetched successfully',
+            responseCount: result.rowCount,
+            response: result.rows
+        })
     } catch (error) {
         console.log(error)
         res.status(400).send({ status: 'FAILED', message:'Error while fetching data', error: error.message })
