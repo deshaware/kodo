e13c47ee73ba8a9c75a67ef2bf0e960b0883f468
Swapnil Deshaware <deshaware.swapnil@gmail.com>
2020-08-16 23:39:15 +0530

    Persisted Reducer Added

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import { Provider } from 'react-redux';
-import store from './store';
+import store, {persistor} from './store';
 import { PersistGate } from 'redux-persist/integration/react';
 
 
--- client/src/store.js
+++ client/src/store.js
@@ -1,19 +1,35 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+
+//persist
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 //initial state
 const initialState = {};
 
 const middleware = [thunk];
 
+//persited and nonpersistedStore
+// const allReducers = combineReducers({
+//     persistedStore: persistReducer({ ...persistConfig, storage }, reducer)
+// })
+
 const store = createStore(
-    rootReducer,
+    persistedReducer,
     initialState,
     compose(
         applyMiddleware(...middleware),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
+export const persistor = persistStore(store)
 
 export default store;
